73ecc3e7923868869ff9ad9796b6eb1d361d7a95
Oteri Prince <oteriprince01@gmail.com>
2024-06-07 12:42:00 +0100

    changes

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://express-realestate.onrender.com/api/v1",
+  baseURL: "https://express-h3l3.onrender.com/api/v1",
   withCredentials: true,
 });
 
